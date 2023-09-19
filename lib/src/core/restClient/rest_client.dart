8f5c4a2032eaf87a26958f783486a39eff925632
Adryel Klever <adryelklever3m@hotmail.com>
2023-09-19 11:41:33 -0300

    Conflito

--- lib/src/core/restClient/rest_client.dart
+++ lib/src/core/restClient/rest_client.dart
@@ -1,10 +1,11 @@
+import 'package:agenda_clinica/src/core/restClient/interceptors/auth_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 
 final class RestClient extends DioForNative {
   RestClient()
       : super(BaseOptions(
-            baseUrl: 'http://192.168.1.5:8080',
+            baseUrl: 'http://192.168.100.100:8080',
             connectTimeout: const Duration(seconds: 10),
             receiveTimeout: const Duration(seconds: 60))) {
     interceptors.addAll([
@@ -12,6 +13,7 @@ final class RestClient extends DioForNative {
         requestBody: true,
         responseBody: true,
       ),
+      AuthInterceptor(),
     ]);
   }
 
