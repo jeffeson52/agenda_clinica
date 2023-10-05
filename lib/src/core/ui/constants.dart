ca3f2c550e451fd50e7772dd0636ea1155b84371
JeFF <jeffeson.a.mor5@gmail.com>
2023-10-05 15:24:51 -0300

    Correção de imagem

--- lib/src/core/ui/constants.dart
+++ lib/src/core/ui/constants.dart
@@ -14,7 +14,7 @@ sealed class ImageConstants {
 }
 
 sealed class ColorsConstants {
-  static const brow = Color(0xFFB07B01);
+  static const brow = Color.fromARGB(255, 219, 41, 106);
   static const grey = Color(0xFF999999);
   static const greyLight = Color(0xFFE6E2E9);
   static const red = Color(0xFFEB1212);
